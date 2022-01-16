190df9dab3b306c2f6423418e4fc266364fc4a73
Tasza33 <91521102+Tasza33@users.noreply.github.com>
2022-01-16 16:39:34 +0300

    Delete test_items.py

--- test_items.py
+++
@@ -1,7 +0,0 @@
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
-def test_button_add_to_basket_present(browser):
-    browser.get(link)
-    add_button = browser.find_elements_by_class_name('btn-add-to-basket')
-    assert add_button, 'basket button not found'
