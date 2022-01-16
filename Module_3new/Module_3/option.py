5de9497ea85916a63385162a5e36e7b151368683
Tasza33 <91521102+Tasza33@users.noreply.github.com>
2022-01-16 16:37:00 +0300

    Delete Module_3new/Module_3 directory

--- Module_3new/Module_3/option.py
+++
@@ -1,5 +0,0 @@
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-browser = webdriver.Chrome(options=options)
